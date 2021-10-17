7e38b2689ff1e5cd913ae688462298980b2f8d86
Dmitry Ivanov <di.ivanov2018@gmail.com>
2021-10-17 23:06:33 +0300

    Configure eslint with typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,23 @@ module.exports = {
     es2021: true,
     "jest/globals": true,
   },
-  extends: ["airbnb-base", "prettier"],
+  extends: [
+    "airbnb-base",
+    "plugin:jest/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["jest"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["jest", "@typescript-eslint"],
   rules: {
     "import/prefer-default-export": "off",
     "max-len": ["error", { ignoreComments: true }],
     "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
+    "import/no-unresolved": "off",
+    "import/extensions": ["warn", "never"],
   },
 };
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,7 @@
 import { sum } from "./index";
 
-describe("", () => {
-  it("", () => {
+describe("Example tests", () => {
+  it("Tests for sum function", () => {
     expect(typeof sum).toBe("function");
     expect(sum(1, 2)).toEqual(3);
   });
