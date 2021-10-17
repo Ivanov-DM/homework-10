7f838a67949303a447e57a0b714685cba305528d
Dmitry Ivanov <di.ivanov2018@gmail.com>
2021-10-17 23:17:36 +0300

    COnfigure jest with typescript

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "bundle.js",
