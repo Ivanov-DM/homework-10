4d2edae94231ae77d21e1cc4ba72d50b6e50d773
Dmitry Ivanov <di.ivanov2018@gmail.com>
2021-10-18 00:11:04 +0300

    Fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export function sum(a: number, b: number): number {
   return a + b;
 }
-const app: HTMLDivElement = document.querySelector(".app") as HTMLDivElement;
-app.innerText = `${sum(2, 2)}`;
+(document.querySelector(".app") as HTMLDivElement).innerText = `${sum(2, 2)}`;
