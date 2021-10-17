d99ae09cdb7afe9ebfd23ac17954da572cb4cdf2
Dmitry Ivanov <di.ivanov2018@gmail.com>
2021-10-18 00:18:15 +0300

    Add app.ts

---
+++ src/app.ts
@@ -0,0 +1,3 @@
+import { sum } from "./index";
+
+(document.querySelector(".app") as HTMLDivElement).innerText = `${sum(2, 2)}`;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export function sum(a: number, b: number): number {
   return a + b;
 }
-(document.querySelector(".app") as HTMLDivElement).innerText = `${sum(2, 2)}`;
