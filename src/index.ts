183c6fa5dcf0feb8823a818e4db267f54b54be9a
Dmitry Ivanov <di.ivanov2018@gmail.com>
2021-10-17 23:33:45 +0300

    Add sanity-check

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export function sum(a: number, b: number): number {
   return a + b;
 }
-const app = document.querySelector('.app') as HTMLDivElement;
-app.innerHTML = '' + sum(2,2);
+const app = document.querySelector(".app") as HTMLDivElement;
+app.innerHTML = `${sum(2, 2)}`;
