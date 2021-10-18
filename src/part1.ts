0ca7ab8cfc98d4a1b6c0b2aca951d4650952c413
Dmitry Ivanov <di.ivanov2018@gmail.com>
2021-10-18 23:52:49 +0300

    Fix part1.ts

--- src/part1.ts
+++ src/part1.ts
@@ -6,14 +6,3 @@ export function curry(func: (...param: number[]) => number) {
     return func(...args);
   };
 }
-
-// function sum(a: number, b: number, c: number, d: number, e: number): number {
-//     return a + b + c + d + e;
-// }
-//
-// const currySum = curry(sum);
-// console.log(currySum(1, 2, 3, 4, 5)); // 15
-// console.log(currySum(2, 3, 4)(5, 6)); // 20
-// console.log(currySum(3, 4)(5, 6)(7)); // 25
-// console.log(currySum(4, 5)(6)(7, 8)); // 30
-// console.log(currySum(5)(6)(7)(8)(9)); // 35
